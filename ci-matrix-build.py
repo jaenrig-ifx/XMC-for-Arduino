56afac09b34f0a329b08d8f026e7bfbd01fae472
jaenrig-ifx <enriquezgarcia.external@infineon.com>
2025-04-22 17:36:39 +0200

    ci-matrix-build.py: WIP Added ci-matrix-build temporarily.
    Signed-off-by: jaenrig-ifx <enriquezgarcia.external@infineon.com>

--- ci-matrix-build.py
+++ ci-matrix-build.py
@@ -1,3 +1,24 @@
 
-matrix = '{ "os": [ "ubuntu-latest", "windows-latest" ], "fqbn": [ "Infineon:xmc:XMC1100_Boot_Kit", "Infineon:xmc:XMC1100_XMC2GO", "Infineon:xmc:XMC1300_Boot_Kit", "Infineon:xmc:XMC4200_Platform2GO", "Infineon:xmc:XMC4400_Platform2GO", "Infineon:xmc:XMC4700_Relax_Kit", "Infineon:xmc:XMC1400_XMC2GO", "Infineon:xmc:XMC1400_Arduino_Kit" ] }' 
+
+# check command lines arguments 
+# The third value is the os name
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python ci-matrix-build.py <os>")
+    sys.exit(1)
+
+os = sys.argv[1]
+
+if os == "ubuntu-latest":
+    matrix = '{ "fqbn": [ "Infineon:xmc:XMC1100_Boot_Kit", "Infineon:xmc:XMC1100_XMC2GO", "Infineon:xmc:XMC1300_Boot_Kit", "Infineon:xmc:XMC4200_Platform2GO", "Infineon:xmc:XMC4400_Platform2GO", "Infineon:xmc:XMC4700_Relax_Kit", "Infineon:xmc:XMC1400_XMC2GO", "Infineon:xmc:XMC1400_Arduino_Kit" ] }' 
+elif os == "windows-latest":
+    matrix = '{ "fqbn": [ "Infineon:xmc:XMC1100_Boot_Kit", "Infineon:xmc:XMC1100_XMC2GO", "Infineon:xmc:XMC1300_Boot_Kit", "Infineon:xmc:XMC4200_Platform2GO", "Infineon:xmc:XMC4400_Platform2GO", "Infineon:xmc:XMC4700_Relax_Kit", "Infineon:xmc:XMC1400_XMC2GO", "Infineon:xmc:XMC1400_Arduino_Kit" ] }' 
+elif os == "macos-latest":
+    matrix = '{ "fqbn": [ ] }'
+else:
+    print("Unknown OS: " + os)
+    sys.exit(1)
+
+#matrix = '{ "os": [ "ubuntu-latest", "windows-latest" ], "fqbn": [ "Infineon:xmc:XMC1100_Boot_Kit", "Infineon:xmc:XMC1100_XMC2GO", "Infineon:xmc:XMC1300_Boot_Kit", "Infineon:xmc:XMC4200_Platform2GO", "Infineon:xmc:XMC4400_Platform2GO", "Infineon:xmc:XMC4700_Relax_Kit", "Infineon:xmc:XMC1400_XMC2GO", "Infineon:xmc:XMC1400_Arduino_Kit" ] }' 
 print(matrix)
