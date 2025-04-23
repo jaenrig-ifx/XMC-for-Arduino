931b574f2d39931eb974c38fec9cec29f132cc4e
jaenrig-ifx <enriquezgarcia.external@infineon.com>
2025-04-23 17:39:57 +0200

    .github/workflows/compile-examples.yml: WIP.
    Signed-off-by: jaenrig-ifx <enriquezgarcia.external@infineon.com>

--- ci-matrix-build.py
+++ ci-matrix-build.py
@@ -8,27 +8,27 @@ import sys
 #     print("Usage: python ci-matrix-build.py <os> --check")
 #     sys.exit(1)
 
-os = sys.argv[1]
+# os = sys.argv[1]
 
-if (len(sys.argv) == 3 and sys.argv[2] == "--check"):
+# if (len(sys.argv) == 3 and sys.argv[2] == "--check"):
 
-    # check if the os is valid
-    if os == "ubuntu-latest" or os == "windows-latest":
-        print('true')
-    else:
-        print('false')
-        sys.exit(0)
+#     # check if the os is valid
+#     if os == "ubuntu-latest" or os == "windows-latest":
+#         print('true')
+#     else:
+#         print('false')
+#         sys.exit(0)
 
-else:
-    if os == "ubuntu-latest":
-        matrix = '{ "fqbn": [ "Infineon:xmc:XMC1100_Boot_Kit", "Infineon:xmc:XMC1100_XMC2GO", "Infineon:xmc:XMC1300_Boot_Kit", "Infineon:xmc:XMC4200_Platform2GO", "Infineon:xmc:XMC4400_Platform2GO", "Infineon:xmc:XMC4700_Relax_Kit", "Infineon:xmc:XMC1400_XMC2GO", "Infineon:xmc:XMC1400_Arduino_Kit" ], "example": ["libraries/LED/examples/SimpleLED/SimpleLED.ino"] }' 
-    elif os == "windows-latest":
-        matrix = '{ "fqbn": [ "Infineon:xmc:XMC1100_Boot_Kit", "Infineon:xmc:XMC1100_XMC2GO", "Infineon:xmc:XMC1300_Boot_Kit", "Infineon:xmc:XMC4200_Platform2GO", "Infineon:xmc:XMC4400_Platform2GO", "Infineon:xmc:XMC4700_Relax_Kit", "Infineon:xmc:XMC1400_XMC2GO", "Infineon:xmc:XMC1400_Arduino_Kit" ], "example": ["libraries/LED/examples/SimpleLED/SimpleLED.ino"] }' 
-    elif os == "macos-latest":
-        matrix = '{ "fqbn": [ ] }'
-    else:
-        print("Unknown OS: " + os)
-        sys.exit(1)
+# else:
+#     if os == "ubuntu-latest":
+#         matrix = '{ "fqbn": [ "Infineon:xmc:XMC1100_Boot_Kit", "Infineon:xmc:XMC1100_XMC2GO", "Infineon:xmc:XMC1300_Boot_Kit", "Infineon:xmc:XMC4200_Platform2GO", "Infineon:xmc:XMC4400_Platform2GO", "Infineon:xmc:XMC4700_Relax_Kit", "Infineon:xmc:XMC1400_XMC2GO", "Infineon:xmc:XMC1400_Arduino_Kit" ], "example": ["libraries/LED/examples/SimpleLED/SimpleLED.ino"] }' 
+#     elif os == "windows-latest":
+#         matrix = '{ "fqbn": [ "Infineon:xmc:XMC1100_Boot_Kit", "Infineon:xmc:XMC1100_XMC2GO", "Infineon:xmc:XMC1300_Boot_Kit", "Infineon:xmc:XMC4200_Platform2GO", "Infineon:xmc:XMC4400_Platform2GO", "Infineon:xmc:XMC4700_Relax_Kit", "Infineon:xmc:XMC1400_XMC2GO", "Infineon:xmc:XMC1400_Arduino_Kit" ], "example": ["libraries/LED/examples/SimpleLED/SimpleLED.ino"] }' 
+#     elif os == "macos-latest":
+#         matrix = '{ "fqbn": [ ] }'
+#     else:
+#         print("Unknown OS: " + os)
+#         sys.exit(1)
 
-    # matrix = '{ "os": [ "ubuntu-latest", "windows-latest" ], "fqbn": [ "Infineon:xmc:XMC1100_Boot_Kit", "Infineon:xmc:XMC1100_XMC2GO", "Infineon:xmc:XMC1300_Boot_Kit", "Infineon:xmc:XMC4200_Platform2GO", "Infineon:xmc:XMC4400_Platform2GO", "Infineon:xmc:XMC4700_Relax_Kit", "Infineon:xmc:XMC1400_XMC2GO", "Infineon:xmc:XMC1400_Arduino_Kit" ], "example": ["libraries/LED/examples/SimpleLED/SimpleLED.ino"] }' 
-    print(matrix)
+matrix = '{ "fqbn": [ "Infineon:xmc:XMC1100_Boot_Kit", "Infineon:xmc:XMC1100_XMC2GO", "Infineon:xmc:XMC1300_Boot_Kit", "Infineon:xmc:XMC4200_Platform2GO", "Infineon:xmc:XMC4400_Platform2GO", "Infineon:xmc:XMC4700_Relax_Kit", "Infineon:xmc:XMC1400_XMC2GO", "Infineon:xmc:XMC1400_Arduino_Kit" ], "example": ["libraries/LED/examples/SimpleLED/SimpleLED.ino"] }' 
+print(matrix)
